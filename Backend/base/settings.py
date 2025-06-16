794dd896aa4be4a2801299c2b80452b4bbc8e5f8
Pradeep <pg649599@gmail.com>
2025-06-17 00:37:06 +0530

    jwt config

--- Backend/base/settings.py
+++ Backend/base/settings.py
@@ -107,8 +107,8 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'ACCESS_TOKEN_LIFETIME': timedelta(days=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=60),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "AUTH_HEADER_TYPES": ("Bearer",),
