e341ba05967990cc7517dfdd5b5296ba1f5af19c
Pradeep <pg649599@gmail.com>
2025-06-17 14:17:19 +0530

    Initialize Group model

--- Backend/apps/groups/config.py
+++ Backend/apps/groups/config.py
@@ -4,3 +4,8 @@ class GroupJoinExitConfig(DjangoChoices):
 
     join = ChoiceItem('join', 'Join')
     exit = ChoiceItem('exit', 'Exit')
+
+class GroupRoleTypeChoices(DjangoChoices):
+
+    admin = ChoiceItem("admin","Admin")
+    member = ChoiceItem("member", "member")
--- Backend/apps/groups/models.py
+++ Backend/apps/groups/models.py
@@ -3,13 +3,22 @@ from django.db import models
 from apps.common.models import BaseModel
 from apps.common.config import COMMAN_CHAR_MAX_LENGTH, COMMAN_DEFAULT_NULL_BLANK_CONFIG, COMMAN_DEFAULT_NULL_CONFIG
 from apps.access.models import User
+from apps.groups.config import GroupRoleTypeChoices
 
 class Group(BaseModel):
 
     name = models.CharField(max_length=COMMAN_CHAR_MAX_LENGTH)
-    members = models.ManyToManyField(User)
     description = models.TextField(**COMMAN_DEFAULT_NULL_BLANK_CONFIG)
-    admin = models.ManyToManyField(User, related_name="related_group_admins")
 
     class Meta(BaseModel.Meta):
         default_related_name = "related_groups"
+
+
+class GroupMembers(BaseModel):
+
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    role = models.CharField(choices=GroupRoleTypeChoices, default=GroupRoleTypeChoices.member)
+    
+    class Meta(BaseModel.Meta):
+        default_related_name = "related_group_roles"
